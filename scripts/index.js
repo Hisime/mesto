d888f325bcc7cc1973acdb2fcb9d85a0262e0b47
Hisime <rihisime@gmail.com>
2022-12-15 18:44:43 +0400

    Реализовано добавление карточек из массива, удаление карточки и добавление лайка

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,58 @@
-let popup = document.querySelector('.popup');
-let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput =  formElement.querySelector('.popup__input_type_job');
-let closeButton = popup.querySelector('.popup__close');
-let editButton = document.querySelector('.profile__edit');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
+const popup = document.querySelector('.popup');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput =  formElement.querySelector('.popup__input_type_job');
+const closeButton = popup.querySelector('.popup__close');
+const editButton = document.querySelector('.profile__edit');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const template = document.querySelector('#gallery-item-template');
+const gallery = document.querySelector('.gallery');
+
+for (let i = 0; i < initialCards.length; i++) {
+  const galleryItem = template.content.querySelector('.gallery__item').cloneNode(true);
+  galleryItem.querySelector('.gallery__city').textContent = initialCards[i].name;
+  galleryItem.querySelector('.gallery__image').src = initialCards[i].link;
+  gallery.append(galleryItem);
+  const deleteButton = galleryItem.querySelector('.gallery__delete-button');
+  deleteButton.addEventListener('click', (event) => {
+    event.target.closest('.gallery__item').remove();
+  });
+  const favoriteButton = galleryItem.querySelector('.gallery__favorite');
+  favoriteButton.addEventListener('click', (event) => {
+    event.target.classList.toggle('gallery__favorite_active');
+  });
+}
+
 
 function closePopup () {
   popup.classList.remove('popup_opened');
